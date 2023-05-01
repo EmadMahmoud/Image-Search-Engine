f3b2e05933140e67730def21363377905af7e911
EmadMahmoud <emadmahmoud289@gmail.com>
2023-05-01 08:03:57 +0300

    set a limit for tensorflow

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,11 +1,16 @@
 #imports
 # Data hadling
 import numpy as np
-
-#modeling
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+# modeling
 from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2, preprocess_input
 from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing import image
+
+
 
 class FeatureExtractor:
    
