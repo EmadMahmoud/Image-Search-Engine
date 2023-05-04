21d0b19098f30b8fc5e0ecf7d2061044d66e1545
EmadMahmoud <emadmahmoud289@gmail.com>
2023-05-04 21:14:09 +0300

    remove the web endpoint

--- app.py
+++ app.py
@@ -25,39 +25,39 @@ app.config[
 api = Api(app)
 api.register_blueprint(Searchblueprint)
 
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        file = request.files['query_img']
-
-        if not file:
-            return
-
-        # Save query image
-        st = time.time()
-
-        img = Image.open(file.stream)  # PIL image
-        # uploaded_img_path = data.uploaded_path+'/' +datetime.now().isoformat().replace(":", ".") + "_" + file.filename
-        # img.save(uploaded_img_path) # uncomment this if you want to save the file into uploaded file
-
-        # Run search
-        try:
-            ids, dists = Search.search(img)
-            paths = [data.imgs_path+'/'+str(id)+'.jpg' for id in ids]
-            scores = [x for x in zip(dists, paths, ids)]
-
-            et = time.time()
-            print(f'Excution time = {et - st} Seconds')
-            return render_template('index.html',
-                                   # query_path=uploaded_img_path,
-                                   scores=scores)
-        except:
-            return render_template('index.html',
-                                   scores=[])
-    else:
-        return render_template('index.html')
-
-
-if __name__=="__main__":
-    app.run("0.0.0.0")
+#
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         file = request.files['query_img']
+#
+#         if not file:
+#             return
+#
+#         # Save query image
+#         st = time.time()
+#
+#         img = Image.open(file.stream)  # PIL image
+#         # uploaded_img_path = data.uploaded_path+'/' +datetime.now().isoformat().replace(":", ".") + "_" + file.filename
+#         # img.save(uploaded_img_path) # uncomment this if you want to save the file into uploaded file
+#
+#         # Run search
+#         try:
+#             ids, dists = Search.search(img)
+#             paths = [data.imgs_path+'/'+str(id)+'.jpg' for id in ids]
+#             scores = [x for x in zip(dists, paths, ids)]
+#
+#             et = time.time()
+#             print(f'Excution time = {et - st} Seconds')
+#             return render_template('index.html',
+#                                    # query_path=uploaded_img_path,
+#                                    scores=scores)
+#         except:
+#             return render_template('index.html',
+#                                    scores=[])
+#     else:
+#         return render_template('index.html')
+#
+#
+# if __name__=="__main__":
+#     app.run("0.0.0.0")
