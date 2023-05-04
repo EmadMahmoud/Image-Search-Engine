61237c7801d06a3ce92f5f80c355cf99c0e592d3
EmadMahmoud <emadmahmoud289@gmail.com>
2023-05-04 18:44:56 +0300

    adjust the base model used for feature_extracion

--- app.py
+++ app.py
@@ -3,14 +3,6 @@ from datetime import datetime
 from flask import Flask, request, render_template
 from flask_smorest import Api
 from resources.searchEP import blp as Searchblueprint
-import resource
-
-# set resource limit to 1 GB (in bytes)
-limit = 256 * 1024 * 1024
-resource.setrlimit(resource.RLIMIT_AS, (limit, limit))
-
-# your Flask application code goes here
-
 from Data import Data
 import time
 
@@ -34,38 +26,38 @@ api = Api(app)
 api.register_blueprint(Searchblueprint)
 
 
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     if request.method == 'POST':
-#         file = request.files['query_img']
-#
-#         if not file:
-#             return
-#
-#         # Save query image
-#         st = time.time()
-#
-#         img = Image.open(file.stream)  # PIL image
-#         # uploaded_img_path = data.uploaded_path+'/' +datetime.now().isoformat().replace(":", ".") + "_" + file.filename
-#         # img.save(uploaded_img_path) # uncomment this if you want to save the file into uploaded file
-#
-#         # Run search
-#         try:
-#             ids, dists = Search.search(img)
-#             paths = [data.imgs_path+'/'+str(id)+'.jpg' for id in ids]
-#             scores = [x for x in zip(dists, paths, ids)]
-#
-#             et = time.time()
-#             print(f'Excution time = {et - st} Seconds')
-#             return render_template('index.html',
-#                                    # query_path=uploaded_img_path,
-#                                    scores=scores)
-#         except:
-#             return render_template('index.html',
-#                                    scores=[])
-#     else:
-#         return render_template('index.html')
-#
-#
-# if __name__=="__main__":
-#     app.run("0.0.0.0")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['query_img']
+
+        if not file:
+            return
+
+        # Save query image
+        st = time.time()
+
+        img = Image.open(file.stream)  # PIL image
+        # uploaded_img_path = data.uploaded_path+'/' +datetime.now().isoformat().replace(":", ".") + "_" + file.filename
+        # img.save(uploaded_img_path) # uncomment this if you want to save the file into uploaded file
+
+        # Run search
+        try:
+            ids, dists = Search.search(img)
+            paths = [data.imgs_path+'/'+str(id)+'.jpg' for id in ids]
+            scores = [x for x in zip(dists, paths, ids)]
+
+            et = time.time()
+            print(f'Excution time = {et - st} Seconds')
+            return render_template('index.html',
+                                   # query_path=uploaded_img_path,
+                                   scores=scores)
+        except:
+            return render_template('index.html',
+                                   scores=[])
+    else:
+        return render_template('index.html')
+
+
+if __name__=="__main__":
+    app.run("0.0.0.0")
--- feature_extractor.py
+++ feature_extractor.py
@@ -1,68 +1,73 @@
-#imports
+# imports
 # Data hadling
 import numpy as np
-import tensorflow as tf
 
-import os
 # modeling
-from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2, preprocess_input
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input as mobilenet_preprocess_input
+from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input as inception_preprocess_input
 from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing import image
 
 
 class FeatureExtractor:
 
-    def __init__(self):
-        # to make tensorflow uses less memory
-        # os.environ['TF_DISABLE_BATCHED_EXECUTION'] = '1'
-        # tf.keras.mixed_precision.set_global_policy('mixed_float16')
+    def __init__(self, model_type='mobilenet'):
+        self.model_type = model_type
+        if self.model_type == 'inception':
+            model = InceptionV3(weights='imagenet', include_top=False)
+            self.preprocess_input = inception_preprocess_input
+        elif self.model_type == 'mobilenet':
+            model = MobileNetV2(weights='imagenet', include_top=False)
+            self.preprocess_input = mobilenet_preprocess_input
+        else:
+            raise Exception('Unknown model type')
 
-        base_model = InceptionResNetV2(weights = 'imagenet', include_top = False)
-        self.model = Model(inputs= base_model.input, outputs = base_model.output)
+        self.base_model = Model(inputs=model.input, outputs=model.output)
+
+        models_shape_map = {'inception': 299, 'mobilenet': 224}
+        self.model_shape = models_shape_map[self.model_type]
 
         # test run
-        # self.model.predict(np.empty([1, 299, 299, 3]))
-
-
+        self.base_model.predict(np.empty([1, self.model_shape, self.model_shape, 3]))
 
     def extract_image(self, img):
         """
-        wrapper for self.extract_batch to extract deep features from one input image using InceptionResNetV2
+        wrapper for self.extract_batch to extract deep features from one input image using the base model
         Args:
             img: image from tensorflow.keras.preprocessing.image.load_img(path)
         returns:
-            features: (np.ndarray) includes deep features      
+            features: (np.ndarray) includes deep features
         """
         return self.extract_batch([img])
-    
-    def extract_batch(self,batch):
+
+    def extract_batch(self, batch):
         """
-        extract deep features from input batch using InceptionResNetV2
+        extract deep features from input batch using the base model
         Args:
             batch: (np.ndarray) of (image) from tensorflow.keras.preprocessing.image.load_img(path)
         returns:
-            features: (np.ndarray) includes deep features      
+            features: (np.ndarray) includes deep features
         """
         batch_array = self.preprocess_batch(batch)
-        features = self.model.predict(batch_array)
-        features = features.reshape(-1,np.prod(features.shape[1:])) #flatten each example 
-        normalized_features = features / np.linalg.norm(features, axis=1,keepdims=True)
+        features = self.base_model.predict(batch_array)
+        features = features.reshape(-1, np.prod(features.shape[1:]))  # flatten each example
+        normalized_features = features / np.linalg.norm(features, axis=1, keepdims=True)
         return normalized_features
-     
+
     def preprocess_batch(self, batch):
         """
-        preprocess batch of images for InceptionResNetV2
+        preprocess batch of images for the model
         Args:
             batch:(np.ndarray) of (image) from tensorflow.keras.preprocessing.image.load_img(path)
         returns:
-            preprocessed_batch: (np.ndarray)  represent the preprocessed batch      
+            preprocessed_batch: (np.ndarray)  represent the preprocessed batch
         """
-        batch_array = np.empty((len(batch),299,299,3))
-        for i,img in enumerate(batch):
-            img = img.resize((299,299)) # (299,299) is InceptionResNetV2 input shape
-            img.convert('RGB')
-            img_array = image.img_to_array(img) #convert it into np array (H X W X C)
+        batch_array = np.empty((len(batch), self.model_shape, self.model_shape, 3))
+        for i, img in enumerate(batch):
+            img = img.resize((self.model_shape, self.model_shape))
+            img = img.convert('RGB')
+            img_array = image.img_to_array(img)  # convert it into np array (H X W X C)
             batch_array[i] = img_array
 
-        preprocessed_batch = preprocess_input(batch_array)
+        preprocessed_batch = self.preprocess_input(batch_array)
         return preprocessed_batch
